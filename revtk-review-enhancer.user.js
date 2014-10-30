6c0d3381bed4ade3b85ad0d80aec6a84fe90fc9c
Eliott Wiener <eliottwiener@gmail.com>
2014-10-30 07:25:18 -0400

    Switching works

--- revtk-review-enhancer.user.js
+++ revtk-review-enhancer.user.js
@@ -4,204 +4,377 @@
 // @version      0.1
 // @description  Allow some basic customization of the card appearance in reviews.
 // @author       Eliott Wiener
-// @match        http://kanji.koohii.com/review*
+// @include      http://kanji.koohii.com/review*
 // @grant        none
 // ==/UserScript==
+var revtkreviewenhancer = (function(){
+	"use strict";
 
-// Got this list from the CSS on beta.jisho.org
-var gothic_fonts = [
-    "HiraKakuPro-W3",
-    "Hiragino Kaku Gothic Pro W3",
-    "Hiragino Kaku Gothic Pro",
-    "ヒラギノ角ゴ Pro W3",
-    "メイリオ",
-    "Meiryo",
-    "游ゴシック",
-    "YuGothic",
-    "ＭＳ Ｐゴシック",
-    "MS PGothic",
-    "ＭＳ ゴシック",
-    "MS Gothic",
-    "sans",
-    "sans-serif"
-];
+	var add_image,
+	add_link,
+	add_text,
+	build_enhancements,
+	card_state_changed,
+	configs_key,
+	current_config_key,
+	default_configs,
+	default_fonts,
+	destroy_enhancements,
+	expand_template,
+	first_call,
+	get_configs,
+	get_current_config,
+	get_current_config_name,
+	gothic_fonts,
+	initialize,
+	log,
+	logging,
+	ready,
+	readyp,
+	set_current_config_name,
+	save_config,
+	stringify_fonts,
+	template_data,
+	template_var_mapping,
+	update_functions,
+	update_template_data,
+	version,
+	wait;
 
-var default_config = {
-    "text": [
-        {
-            "template": "%k",
-            "font": JSON.stringify(["KanjiStrokeOrders"].concat(gothic_fonts)),
-            "enabled": false
-        },
-        {
-            "template": "%k",
-            "font": JSON.stringify(gothic_fonts),
-            "enabled": false
-        }
-    ],
-    "images": [
-        {
-            "url_template": "https://cdn.rawgit.com/KanjiVG/kanjivg/r20140816/kanji/0%uh.svg",
-            "width": "50%",
-            "height": "50%",
-            "alt_template": "%k",
-            "alt_font": JSON.stringify(["KanjiStrokeOrders"]),
-            "enabled": true
-        }
-    ],
-    "links": [
-        {
-            "url_template": "http://beta.jisho.org/search/%k%20%23kanji",
-            "text_template": "Look up %k on beta.jisho.org",
-            "font": JSON.stringify(gothic_fonts),
-            "enabled": true
-        }
-    ]
-};
+	version = 0.1;
 
-var template_data;
-var template_var_mapping = {
-    "h": "keyword",
-    "k": "kanji",
-    "f": "frame",
-    "ud": "unicode_codepoint_dec",
-    "uh": "unicode_codepoint_hex"
-};
-var update_template_data = function(){
-    var data = window.rkKanjiReview.oReview.getFlashcardData();
-    var keyword = data["keyword"].replace(/>.*?<\/a>$/,/\1/);
-    var kanji = data["kanji"];
-    var frame = data["frameNum"];
-    var unicode_codepoint_dec = kanji.charCodeAt(0);
-    var unicode_codepoint_hex = unicode_codepoint_dec.toString(16);
-    template_data = {
-        "keyword": keyword,
-        "kanji": kanji,
-        "frame": frame,
-        "unicode_codepoint_dec": unicode_codepoint_dec,
-        "unicode_codepoint_hex": unicode_codepoint_hex
-    }
-};
-var expand_template = function(template){
-    var expanded_template = template;
-    Object.keys(template_var_mapping).map(function(v){
-        expanded_template = expanded_template.replace("%"+v, template_data[template_var_mapping[v]]);
-    });
-    return expanded_template;
-};
+	logging = true;
+	log = function(message){
+		if(logging){
+			console.log("revtk-review-enhancer: " + message);
+		}
+	};
 
-var add_text = function(template, font){
-    var text = document.createElement("span");
-    text.style.fontFamily = font;
-    text.lang = "ja";
-    document.getElementById("text_box").appendChild(text);
-    var update = function(){
-        text.innerText = expand_template(template);
-    };
-    update_functions.push(update);
-};
+	log("started");
 
-var add_link = function(url_template, text_template, font){
-    var link = document.createElement("a");
-    link.style.fontSize = "12px";
-    link.style.fontFamily = font;
-    link.target = "_blank";
-    link.className = "JsKeywordLink"; // YUI dark magic
-    document.getElementById("link_box").appendChild(link);
-    var update = function(){
-        link.href = expand_template(url_template);
-        link.innerText = expand_template(text_template);
-    };
-    update_functions.push(update);
-};
+	// Got this list from the CSS on beta.jisho.org
+	gothic_fonts = [
+		"HiraKakuPro-W3",
+		"Hiragino Kaku Gothic Pro W3",
+		"Hiragino Kaku Gothic Pro",
+		"ヒラギノ角ゴ Pro W3",
+		"メイリオ",
+		"Meiryo",
+		"游ゴシック",
+		"YuGothic",
+		"ＭＳ Ｐゴシック",
+		"MS PGothic",
+		"ＭＳ ゴシック",
+		"MS Gothic",
+		"sans",
+		"sans-serif"
+	];
 
-var add_image = function(url_template, alt_template, alt_font, width, height){
-    var image = document.createElement("img");
-    image.style.fontFamily = alt_font;
-    image.style.width = width;
-    image.style.height = height;
-    image.style.display = "block";
-    image.style.margin = "auto";
-    document.getElementById("image_box").appendChild(image);
-    var update = function(){
-        image.src = expand_template(url_template);
-        image.alt = expand_template(alt_template);
-    };
-    update_functions.push(update);
-};
+	default_fonts = [
+		"Hiragino Mincho Pro",
+		"ヒラギノ明朝 Pro W3",
+		"ＭＳ 明朝",
+		"ＭＳ Ｐ明朝",
+		"serif"
+	];
 
-var initialize = function(){
-    console.log("init");
-    var kanjibig = document.getElementById("kanjibig")
-    kanjibig.style.position = "initial !important";
-	kanjibig.style.top = "0px !important";
-    kanjibig.style.marginTop = "0px !important";
-    var original_kanji = document.getElementsByClassName("fcData-kanji")[0];
-    var card = original_kanji.parentNode;
-    original_kanji.style.display = "none";
-    var enhancements = document.createElement("div");
-    enhancements.id = "enhancements";
-    card.appendChild(enhancements);
-    build_enhancements();
-};
+	stringify_fonts = function(fonts){
+		// Unlike chrome, firefox does not
+		// automatically join and quote an
+		// array passed to font-family.
+		var string = "'";
+		string += fonts.join("','");
+		string += "'";
+		return string;
+	};
 
-var destroy_enhancements = function(){
-    enhancements.childNodes.map(function(x){x.remove()});
-    update_functions = [];
-};
+	template_var_mapping = {
+		"fn": "frame_number",
+		"hk": "heisig_keyword",
+		"kj": "kanji",
+		"sc": "stroke_count",
+		"ud": "unicode_codepoint_dec",
+		"uh": "unicode_codepoint_hex",
+		"ub": "unicode_codepoint_bin",
+		"ue": "uri_encoded",
+	};
 
-var config;
-var build_enhancements = function(){
-    var enhancements = document.getElementById("enhancements");
-    var image_box = document.createElement("div");
-    image_box.id = "image_box";
-    enhancements.appendChild(image_box);
-    config["images"].map(function(i){
-        if(i["enabled"]){
-            add_image(i["url_template"], i["alt_template"], JSON.parse(i["alt_font"]), i["width"], i["height"]);
-        }
-    });
-    var text_box = document.createElement("div");
-    text_box.id = "text_box";
-    enhancements.appendChild(text_box);
-    config["text"].map(function(t){
-        if(t["enabled"]){
-            add_text(t["template"], JSON.parse(t["font"]));
-        }
-    });
-    var link_box = document.createElement("div");
-    link_box.id = "link_box";
-    enhancements.appendChild(link_box);
-    config["links"].map(function(l){
-        if(l["enabled"]){
-            add_link(l["url_template"], l["text_template"], JSON.parse(l["font"]));
-        }
-    });
-};
+	default_configs = {
+		"SVG strokes + jisho link":	{
+			"contents": [
+				[
+					{
+						"type": "image",
+						"url_template": "https://cdn.rawgit.com/KanjiVG/kanjivg/r20140816/kanji/0%uh.svg",
+						"alt_template": "%kj",
+						"alt_font": "KanjiStrokeOrders",
+						"flex_grow": 10
+					},
+				],
+				[
+					{
+						"type": "link",
+						"url_template": "http://beta.jisho.org/search/%ue%20%23kanji",
+						"text_template": "Look up %kj on beta.jisho.org",
+						"font": stringify_fonts(gothic_fonts),
+						"font_size": "12px"
+					}
+				]
+			]
+		},
 
-var update_functions = [];
-var card_state_changed = function(){
-    update_template_data();
-    update_functions.map(function(f){f.call();});
-};
+		"Default-like":	{
+			"contents": [
+				[
+					{
+						"type": "text",
+						"template": "%kj",
+						"font": stringify_fonts(default_fonts),
+						"font_size": "15em"
+					},
+				]
+			]
+		},
 
-var config_key = "revtk_review_enhancer_config";
-var configUpdated = function(){
-    localStorage[config_key] = JSON.stringify(config);
-    // re-init card display
-};
-var getConfig = function(){
-    return JSON.parse(localStorage[config_key]);
-};
-var config = default_config; //getConfig() || default_config;
+		"Gothic font": {
+			"contents": [
+				[
+					{
+						"type": "text",
+						"template": "%kj",
+						"font": stringify_fonts(gothic_fonts),
+						"font_size": "30em"
+					}
+				]
+			]
+		},
 
-var first_call = true;
-var orig = window.rkKanjiReview.oReview.setFlashcardState;
-window.rkKanjiReview.oReview.setFlashcardState = function(e){
-    orig.call(this,e);
-    if(first_call){
-        initialize();
-        first_call = false;
-    }
-    card_state_changed();
-};
+		"KanjiStrokeOrders font": {
+			"contents": [
+				[
+					{
+						"type": "text",
+						"template": "%kj",
+						"font": "KanjiStrokeOrders",
+						"font_size": "30em"
+					}
+				]
+			]
+		},
+
+		"Template variables": {
+			"contents": Object.keys(template_var_mapping).map(function(k){
+				return [
+					{
+						"type": "text",
+						"template": template_var_mapping[k] + " (" + k + "): %" + k,
+						"font": default_fonts,
+						"font_size": "12px"
+					}
+				];
+			})
+		}
+	};
+	configs_key = "revtk_review_enhancer_configs";
+	current_config_key = "revtk_review_enhancer_current_config";
+	//if(true || !localStorage[configs_key]){
+		localStorage[configs_key] = JSON.stringify(default_configs);
+	//}
+	get_configs = function(){
+		return JSON.parse(localStorage[configs_key]);
+	};
+	//if(true || !localStorage[current_config_key]){
+		localStorage[current_config_key] = "Template variables" || Object.keys(get_configs())[0];
+	//}
+	get_current_config_name = function(){
+		return localStorage[current_config_key];
+	};
+	get_current_config = function(){
+		return get_configs()[get_current_config_name()];
+	};
+	set_current_config_name = function(config_name){
+		localStorage[current_config_key] = config_name;
+	};
+	save_config = function(config_name, config_contents){
+	}
+
+	update_functions = [];
+
+	update_template_data = function(){
+		var data,
+		frame_number,
+		heisig_keyword,
+		kanji,
+		stroke_count,
+		unicode_codepoint_dec,
+		unicode_codepoint_hex,
+		unicode_codepoint_bin,
+		uri_encoded,
+		data = window.rkKanjiReview.oReview.getFlashcardData();
+		frame_number = data.framenum;
+		heisig_keyword = data.keyword.replace(/[^>]*>(.*?)<\/a>$/,"$1");
+		kanji = data.kanji;
+		stroke_count = data.strokecount;
+		unicode_codepoint_dec = kanji.charCodeAt(0);
+		unicode_codepoint_hex = unicode_codepoint_dec.toString(16);
+		unicode_codepoint_bin = unicode_codepoint_dec.toString(2);
+		uri_encoded = encodeURIComponent(kanji);
+		template_data = {
+			"frame_number": frame_number,
+			"heisig_keyword": heisig_keyword,
+			"kanji": kanji,
+			"stroke_count": stroke_count,
+			"unicode_codepoint_dec": unicode_codepoint_dec,
+			"unicode_codepoint_hex": unicode_codepoint_hex,
+			"unicode_codepoint_bin": unicode_codepoint_bin,
+			"uri_encoded": uri_encoded
+		};
+	};
+	expand_template = function(template){
+		var expanded_template = template;
+		Object.keys(template_var_mapping).map(function(v){
+			expanded_template = expanded_template.replace("%"+v, template_data[template_var_mapping[v]]);
+		});
+		log("expanded template: '" + template + "' -> '" + expanded_template + "'");
+		return expanded_template;
+	};
+
+	add_text = function(text_config){
+		var text = document.createElement("span");
+		text.style.fontFamily = text_config.font;
+		text.style.textAlign = "center";
+		text.style.fontSize = text_config.font_size;
+		text.lang = "ja";
+		document.getElementById("enhancements").appendChild(text);
+		update_functions.push(function(){
+			text.textContent = expand_template(text_config.template);
+		});
+	};
+
+	add_link = function(link_config){
+		var link = document.createElement("a");
+		link.style.fontFamily = link_config.font;
+		link.lang = "ja";
+		link.style.textAlign = "center";
+		link.style.fontSize = link_config.font_size;
+		link.target = "_blank";
+		link.className = "JsKeywordLink"; // YUI dark magic
+		document.getElementById("enhancements").appendChild(link);
+		update_functions.push(function(){
+			link.href = expand_template(link_config.url_template);
+			link.textContent = expand_template(link_config.text_template);
+		});
+	};
+
+	add_image = function(image_config){
+		var image = document.createElement("img");
+		image.style.fontFamily = image_config.alt_font;
+		image.style.display = "inline";
+		image.style.flexGrow = image_config.flex_grow;
+		image.lang = "ja";
+		document.getElementById("enhancements").appendChild(image);
+		update_functions.push(function(){
+			image.src = expand_template(image_config.url_template);
+			image.alt = expand_template(image_config.alt_template);
+		});
+	};
+
+	initialize = function(){
+		var enhancements, config_select;
+		log("initialize");
+		enhancements = document.createElement("div");
+		//.framenum", "strokecount.map(function(x){
+			//document.getElementById(x).style.visibility="visible";
+		//});
+		enhancements.id = "enhancements";
+		enhancements.style.alignItems = "stretch";
+		enhancements.style.display = "flex";
+		enhancements.style.flexDirection = "column";
+		enhancements.style.height = "100%";
+		enhancements.style.justifyContent = "center";
+		enhancements.style.visibility = "hidden";
+		enhancements.style.width = "100%";
+		config_select = document.createElement("select");
+		config_select.id = "config_select";
+		config_select.style.display = "inline";
+		Object.keys(get_configs()).map(function(config_name){
+			var option = document.createElement("option");
+			option.textContent = option.value = config_name;
+			config_select.appendChild(option);
+		});
+		config_select.value = get_current_config_name();
+		config_select.onchange = function(){
+			log("config chosen: " + config_select.value);
+			set_current_config_name(config_select.value);
+			destroy_enhancements();
+			build_enhancements();
+		};
+		document.getElementById("uiFcOptions").appendChild(config_select);
+		document.getElementById("kanjibig").style.display = "none";
+		document.getElementById("uiFcMain").appendChild(enhancements);
+		build_enhancements();
+	};
+
+	card_state_changed = function(){
+		log("card state changed - updating");
+		if(window.rkKanjiReview.oReview.getFlashcardState()){
+			document.getElementById("enhancements").style.visibility = "visible";
+		} else {
+			document.getElementById("enhancements").style.visibility = "hidden";
+		}
+		update_template_data();
+		update_functions.map(function(f){f.call();});
+	};
+
+	destroy_enhancements = function(){
+		log("destroying enhancements");
+		var enhancements = document.getElementById("enhancements");
+		while(enhancements.firstChild){
+			enhancements.removeChild(enhancements.firstChild);
+		}
+		update_functions = [];
+	};
+
+	build_enhancements = function(){
+		log("building enhancements");
+		get_current_config().contents.map(function(row){
+			row.map(function(i){
+				if(i.type === "image"){
+					add_image(i);
+				} else if(i.type === "text"){
+					add_text(i);
+				} else if(i.type === "link"){
+					add_link(i);
+				}
+			});
+		});
+		card_state_changed();
+		log("enhancements built");
+	};
+
+	first_call = true;
+	ready = function(){
+		log("ready");
+		var orig = window.rkKanjiReview.oReview.setFlashcardState;
+		window.rkKanjiReview.oReview.setFlashcardState = function(e){
+			orig.call(this,e);
+			if(first_call){
+				initialize();
+				first_call = false;
+			}
+			card_state_changed();
+		};
+	};
+	readyp = function(){
+		log("checking if ready");
+		return window.rkKanjiReview.oReview;
+	};
+	wait = function(check, callback){
+		if(check()){
+			callback();
+		} else {
+			window.setTimeout(wait, 500, check, callback);
+		}
+	};
+	wait(readyp, ready);
+}());
